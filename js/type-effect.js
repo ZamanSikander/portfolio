cbb0c55be2233b16c93257a2071a596d11c9ff06
ZamanSikander <115521715+ZamanSikander@users.noreply.github.com>
2024-01-30 21:10:08 +0500

    Add files via upload

---
+++ js/type-effect.js
@@ -0,0 +1,8 @@
+// var typed = new typed('#element', {
+//     strings: ['professional web-developer, web-designer,  and professioanl wordpress developer.'],
+//     typeSpeed: 50,
+// });
+var typed = new Typed('#element', {
+    strings: ['professional web-developer, Front-End developer, and professional wordpress developer with a experience of 3 years. '],
+    typeSpeed:30,
+  });
